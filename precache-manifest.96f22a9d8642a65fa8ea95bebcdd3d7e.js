9b18e9e0a7f68e44b7bc4d57f5769d20261d3154
Sofia-Cardozo <sofia11cardozo@gmail.com>
2025-04-16 16:36:07 -0300

    Updates

---
+++ precache-manifest.96f22a9d8642a65fa8ea95bebcdd3d7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72b2ad46318e435fd0025b9baeb6cba",
+    "url": "/Let-so/Mi-app-react/index.html"
+  },
+  {
+    "revision": "581f8eb1f13ae68ca62c",
+    "url": "/Let-so/Mi-app-react/static/css/main.5b89579c.chunk.css"
+  },
+  {
+    "revision": "a6cc2c60adff9483e55f",
+    "url": "/Let-so/Mi-app-react/static/js/2.f6009607.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/Let-so/Mi-app-react/static/js/2.f6009607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6699dc38144bae0b8465",
+    "url": "/Let-so/Mi-app-react/static/js/3.0d41a95b.chunk.js"
+  },
+  {
+    "revision": "581f8eb1f13ae68ca62c",
+    "url": "/Let-so/Mi-app-react/static/js/main.07f7c04d.chunk.js"
+  },
+  {
+    "revision": "1cac08f1aff132d06ab2",
+    "url": "/Let-so/Mi-app-react/static/js/runtime-main.ba2e62aa.js"
+  }
+]);
