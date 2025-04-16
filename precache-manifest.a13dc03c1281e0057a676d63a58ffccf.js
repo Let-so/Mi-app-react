c8991cb00675f7541bb66891340ca3c093bcb750
Sofia-Cardozo <sofia11cardozo@gmail.com>
2025-04-16 16:40:59 -0300

    Updates

---
+++ precache-manifest.a13dc03c1281e0057a676d63a58ffccf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c725d8fd865a7b753dce1d65eb10c1b2",
+    "url": "/Mi-app-react/index.html"
+  },
+  {
+    "revision": "581f8eb1f13ae68ca62c",
+    "url": "/Mi-app-react/static/css/main.5b89579c.chunk.css"
+  },
+  {
+    "revision": "a6cc2c60adff9483e55f",
+    "url": "/Mi-app-react/static/js/2.f6009607.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/Mi-app-react/static/js/2.f6009607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6699dc38144bae0b8465",
+    "url": "/Mi-app-react/static/js/3.0d41a95b.chunk.js"
+  },
+  {
+    "revision": "581f8eb1f13ae68ca62c",
+    "url": "/Mi-app-react/static/js/main.07f7c04d.chunk.js"
+  },
+  {
+    "revision": "03378876c176f79893b8",
+    "url": "/Mi-app-react/static/js/runtime-main.03139764.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Let-so/Mi-app-react/precache-manifest.96f22a9d8642a65fa8ea95bebcdd3d7e.js"
+  "/Mi-app-react/precache-manifest.a13dc03c1281e0057a676d63a58ffccf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Let-so/Mi-app-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mi-app-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
